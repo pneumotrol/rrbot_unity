447eb7ada1a30249c2887061dd95e141fcee965b
Kazuki Hamada <pneumotrol@gmail.com>
2023-09-16 01:01:10 +0900

    Create /joint_states publisher.

--- Assets/Scripts/RosPublisher.cs
+++ Assets/Scripts/RosPublisher.cs
@@ -3,20 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Robotics.ROSTCPConnector;
 using JointStateMsg = RosMessageTypes.Sensor.JointStateMsg;
+using Unity.Robotics.UrdfImporter;
+using UnityEditor;
+using System;
 
 public class RosPublisher : MonoBehaviour
 {
-    ROSConnection ros;
-    public string topicName = "joint_states";
+    [SerializeField]
+    GameObject robot;
+
+    public string topicName = "/joint_states";
     public float publishMessageFrequency = 0.5f;
 
-    private float timeElapsed = 0.0f;
+    ROSConnection ros;
+    float timeElapsed = 0.0f;
+    ArticulationBody[] bodies;
+    JointStateMsg joint_states;
 
-    // start ROS connection
     void Start()
     {
+        // start ROS connection
         ros = ROSConnection.GetOrCreateInstance();
         ros.RegisterPublisher<JointStateMsg>(topicName);
+
+        // get ArticulationBody from GameObject
+        bodies = robot.GetComponentsInChildren<ArticulationBody>(true);
+
+        // initialize joint_states message
+        joint_states = new JointStateMsg();
+        Array.Resize(ref joint_states.name, bodies.Length);
+        Array.Resize(ref joint_states.position, bodies.Length);
+        Array.Resize(ref joint_states.velocity, bodies.Length);
+        Array.Resize(ref joint_states.effort, bodies.Length);
+
+        for (int i = 0; i < bodies.Length; i++)
+        {
+            joint_states.name[i] = bodies[i].name;
+            joint_states.position[i] = (bodies[i].jointPosition.dofCount == 1) ? bodies[i].jointPosition[0] : 0.0;
+            joint_states.velocity[i] = (bodies[i].jointVelocity.dofCount == 1) ? bodies[i].jointVelocity[0] : 0.0;
+            joint_states.effort[i] = (bodies[i].jointForce.dofCount == 1) ? bodies[i].jointForce[0] : 0.0; ;
+        }
+
+        ros.Publish(topicName, joint_states);
     }
 
     // Update is called once per frame
@@ -26,7 +54,13 @@ public class RosPublisher : MonoBehaviour
 
         if (timeElapsed > publishMessageFrequency)
         {
-            JointStateMsg joint_states= new JointStateMsg();
+            for (int i = 0; i < bodies.Length; i++)
+            {
+                joint_states.name[i] = bodies[i].name;
+                joint_states.position[i] = (bodies[i].jointPosition.dofCount == 1) ? bodies[i].jointPosition[0] : 0.0;
+                joint_states.velocity[i] = (bodies[i].jointVelocity.dofCount == 1) ? bodies[i].jointVelocity[0] : 0.0;
+                joint_states.effort[i] = (bodies[i].jointForce.dofCount == 1) ? bodies[i].jointForce[0] : 0.0; ;
+            }
 
             ros.Publish(topicName, joint_states);
 
